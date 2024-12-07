2fe4ec0859a81b44980f00ec5da1d4b13fa8193c
amangupta9454 <ag0567688@gmail.com>
2024-12-07 12:49:13 +0530

    'animation'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        fadeIn: 'fadeIn 2s ease-in-out', // Define fade-in animation with duration and easing
+      },
+    },
     screens: {
       xs: "420px", // Extra small devices (e.g., small phones)
-      sm: "640px", // Default small breakpoint (you can adjust or remove if needed)
+      sm: "640px", // Small breakpoint
       md: "768px", // Medium devices (e.g., tablets)
       lg: "1024px", // Large devices (e.g., laptops)
       xl: "1280px", // Extra-large devices (e.g., desktops)
-      "2xl": "1536px", 
+      "2xl": "1536px", // 2X extra-large devices
     },
   },
   plugins: [],
 };
-
